a51112e144a47d5e832fb5b6ffa500e52c8b5eb7
Magmusacy <xpfk.098@gmail.com>
2022-05-06 17:52:29 +0200

    Create basic User profile show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-
-    unless @user
-      redirect_to root_url, alert: "User not found"
-    end # zobacz jak to zrobic zeby nie throwowalo errora albo zeby to moglo wejsc tylko na swoj profajl???
-    # zobacz na blogu jak to zrobiles zeby nie throwowalo erorr
   end
 end
