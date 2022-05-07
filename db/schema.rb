8b701e046fe761931debceae87eb88c59faf332a
Magmusacy <xpfk.098@gmail.com>
2022-05-07 18:38:34 +0200

    Change database from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_07_153226) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "event_attendings", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
