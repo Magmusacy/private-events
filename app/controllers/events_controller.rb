653cdaa6303b64094bab93e8d520a667e3a463af
Magmusacy <xpfk.098@gmail.com>
2022-05-11 18:31:09 +0200

    Add option for creator to delete/edit the Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,14 +14,36 @@ class EventsController < ApplicationController
     @event = current_user.created_events.build(event_params)
 
     if @event.save
-      redirect_to @event
+      redirect_to @event, notice: "Successfully created the event"
     else
+      flash.now[:alert] = "Something went wrong... Change the highlighted fields"
       render :new
     end
   end
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event, notice: "Successfully updated the event"
+    else
+      flash.now[:alert] = "Something went wrong... Change the highlighted fields"
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.delete
+    redirect_to root_path, notice: "Successfully deleted the event"
   end
 
   private
